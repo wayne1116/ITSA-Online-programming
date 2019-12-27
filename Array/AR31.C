e1f551b0b9f73521ba8ebaa2b363cdd946cd34fa
Wayne Chen <wayne861116@gmail.com>
2019-12-27 22:37:49 +0800

    Update AR31.C

--- Array/AR31.C
+++ Array/AR31.C
@@ -4,63 +4,63 @@
 #define MAX 1024
 int main()
 {
-		char line[MAX], *ptr, *ptr1, *ptr2;
+	char line[MAX], *ptr, *ptr1, *ptr2;
 
-		while(fgets(line, MAX, stdin)!=NULL){
-				ptr1=line;
-				ptr2=line;
-				while(*ptr1!='\0'){
-						if(*ptr1==','){
-								ptr=ptr1+1;
-								ptr1--;
-								while(ptr1!=ptr2){
-										if(isupper(*ptr1))
-												printf("%c", tolower(*ptr1));
-										else if(islower(*ptr1))
-												printf("%c", toupper(*ptr1));
-										else
-												printf("%c", *ptr1);
-										ptr1--;
-								}
-								if(isupper(*ptr1))
-										printf("%c", tolower(*ptr1));
-								else if(islower(*ptr1))
-										printf("%c", toupper(*ptr1));
-								else
-										printf("%c", *ptr1);
-								printf(",");
-								if(*ptr==' ')
-										printf(" ");
-								ptr1=ptr;
-								ptr2=ptr+1;
-						}
-						else if(*ptr1==' '||*ptr1=='.'||*ptr1=='!'){
-								ptr=ptr1;
-								ptr1--;
-								while(ptr1!=ptr2){
-										if(isupper(*ptr1))
-												printf("%c", tolower(*ptr1));
-										else if(islower(*ptr1))
-												printf("%c", toupper(*ptr1));
-										else
-												printf("%c", *ptr1);
-										ptr1--;
-								}
-								if(isupper(*ptr1))
-										printf("%c", tolower(*ptr1));
-								else if(islower(*ptr1))
-										printf("%c", toupper(*ptr1));
-								else
-										printf("%c", *ptr1);
-								printf("%c", *ptr);
-								ptr1=ptr;
-								ptr2=ptr+1;
-						}
-						ptr1++;
+	while(fgets(line, MAX, stdin)!=NULL){
+		ptr1=line;
+		ptr2=line;
+		while(*ptr1!='\0'){
+			if(*ptr1==','){
+				ptr=ptr1+1;
+				ptr1--;
+				while(ptr1!=ptr2){
+					if(isupper(*ptr1))
+						printf("%c", tolower(*ptr1));
+					else if(islower(*ptr1))
+						printf("%c", toupper(*ptr1));
+					else
+						printf("%c", *ptr1);
+					ptr1--;
 				}
-				printf("\n");
+				if(isupper(*ptr1))
+					printf("%c", tolower(*ptr1));
+				else if(islower(*ptr1))
+					printf("%c", toupper(*ptr1));
+				else
+					printf("%c", *ptr1);
+				printf(",");
+				if(*ptr==' ')
+					printf(" ");
+				ptr1=ptr;
+				ptr2=ptr+1;
+			}
+			else if(*ptr1==' '||*ptr1=='.'||*ptr1=='!'){
+				ptr=ptr1;
+				ptr1--;
+				while(ptr1!=ptr2){
+					if(isupper(*ptr1))
+						printf("%c", tolower(*ptr1));
+					else if(islower(*ptr1))
+						printf("%c", toupper(*ptr1));
+					else
+						printf("%c", *ptr1);
+					ptr1--;
+				}
+				if(isupper(*ptr1))
+					printf("%c", tolower(*ptr1));
+				else if(islower(*ptr1))
+					printf("%c", toupper(*ptr1));
+				else
+					printf("%c", *ptr1);
+				printf("%c", *ptr);
+				ptr1=ptr;
+				ptr2=ptr+1;
+			}
+			ptr1++;
 		}
-		return 0;
+		printf("\n");
+	}
+	return 0;
 }
 
 
