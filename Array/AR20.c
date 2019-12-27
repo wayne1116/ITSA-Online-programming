8d35432eb3f30e6a191b22aa51fcf3047c13e74b
Wayne Chen <wayne861116@gmail.com>
2019-12-27 22:14:46 +0800

    Update and rename AR20.C to AR20.c

---
+++ Array/AR20.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define MAX 1024
+int main()
+{
+	char line[MAX], *ptr, *ptr1;
+	while(fgets(line, MAX, stdin)!=NULL){
+		ptr=line;
+		while(*ptr!='\0'){
+			if(*ptr=='\n')
+				break;
+			ptr++;
+		}
+		ptr--;
+		ptr1=line;
+		while(ptr!=ptr1 && ptr1<ptr){
+			if(*ptr!=*ptr1)
+				break;
+			ptr1++;
+			ptr--;
+		}
+		if(ptr1==ptr || ptr1>ptr)
+			printf("yes\n");
+		else
+			printf("no\n");
+	}
+	return 0;
+}
