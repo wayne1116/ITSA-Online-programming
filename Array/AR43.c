919ef2fe97c2ec3a55a75a81c293f66ae0633797
Wayne Chen <wayne861116@gmail.com>
2019-12-28 00:00:29 +0800

    Update and rename AR43.C to AR43.c

---
+++ Array/AR43.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	int row, col, i, j, temp, mat1[10][10];
+	scanf("%d %d", &row, &col);
+	for(i=0; i<row; i++){
+		for(j=0; j<col; j++)
+		   scanf("%d", &mat1[i][j]);
+	}
+	for(i=0; i<row; i++){
+		for(j=0; j<col; j++){
+			scanf("%d", &temp);
+			mat1[i][j]+=temp;
+		}
+	}
+
+	for(i=0; i<row; i++){
+		for(j=0; j<col-1; j++)
+			printf("%d ", mat1[i][j]);
+		printf("%d\n", mat1[i][j]);
+	}
+	return 0;
+}
