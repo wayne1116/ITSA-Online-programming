a8842860ba1e4da39bfa4e85f713f40d68b253df
Wayne Chen <wayne861116@gmail.com>
2019-12-28 00:14:47 +0800

    Update AR47.C

--- Array/AR47.C
+++ Array/AR47.C
@@ -4,27 +4,27 @@
 #define IN  1
 int main()
 {
-		int a, state=OUT;
-		while(scanf("%d", &a)!=EOF){
-				while(a!=0){
-						if(a==1)
-								break;
-						if(a%2==0)
-								a/=2;
-						else if(a%3==0)
-								a/=3;
-						else if(a%5==0)
-								a/=5;
-						else{
-								state=IN;
-								break;
-						}
-				}
-				if(state!=IN)
-						printf("Regular number!\n");
-				else
-						printf("Irregular number!\n");
-				state=OUT;
+	int a, state=OUT;
+	while(scanf("%d", &a)!=EOF){
+		while(a!=0){
+			if(a==1)
+				break;
+			if(a%2==0)
+				a/=2;
+			else if(a%3==0)
+				a/=3;
+			else if(a%5==0)
+				a/=5;
+			else{
+				state=IN;
+				break;
+			}
 		}
-		return 0;
+		if(state!=IN)
+			printf("Regular number!\n");
+		else
+			printf("Irregular number!\n");
+		state=OUT;
+	}
+	return 0;
 }
