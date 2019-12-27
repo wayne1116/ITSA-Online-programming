d53a4bd2168dc3ea57a46663feca6fef210d2438
Wayne Chen <wayne861116@gmail.com>
2019-12-28 00:02:32 +0800

    Update AR44.C

--- Array/AR44.C
+++ Array/AR44.C
@@ -2,21 +2,21 @@
 #include<stdlib.h>
 int main()
 {
-		int row, col, i, j;
-		float number1[10][10], number;
-		while(scanf("%d %d", &row, &col)!=EOF){
-				for(i=0; i<row; i++){
-						for(j=0; j<col; j++){
-								scanf("%f", &number1[i][j]);
-						}
-				}
-				for(i=0; i<row; i++){
-						for(j=0; j<col; j++){
-								scanf("%f", &number);
-								printf("[%.1f]", (number+number1[i][j])/2);
-						}
-						printf("\n");
-				}
+	int row, col, i, j;
+	float number1[10][10], number;
+	while(scanf("%d %d", &row, &col)!=EOF){
+		for(i=0; i<row; i++){
+			for(j=0; j<col; j++){
+				scanf("%f", &number1[i][j]);
+			}
 		}
-		return 0;
+		for(i=0; i<row; i++){
+			for(j=0; j<col; j++){
+				scanf("%f", &number);
+				printf("[%.1f]", (number+number1[i][j])/2);
+			}
+			printf("\n");
+		}
+	}
+	return 0;
 }
