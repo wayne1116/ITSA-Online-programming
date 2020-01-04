648fb0e8ec9edc065a54d1fb9f636997d79ec769
Wayne Chen <wayne861116@gmail.com>
2020-01-04 22:51:48 +0800

    Update CH08.C

--- Comprehensive/CH08.C
+++ Comprehensive/CH08.C
@@ -3,26 +3,24 @@
 #include<ctype.h>
 int main()
 {
-		char line[1024], *ptr;
-		int count, result, number;
-		while(scanf("%s", line)!=EOF){
-				ptr=line; count=1; result=0;
-				while(*ptr){
-						if(count>9)
-								break;
-						if(isdigit(*ptr)){
-								result+=((*ptr-48)*count);
-								count++;
-						}
-						ptr++;
-				}
-				number=atoi(ptr+1);
-				if(result%11==number)
-						printf("Pass\n");
-				else
-						printf("Fail\n");
+	char line[1024], *ptr;
+	int count, result, number;
+	while(scanf("%s", line)!=EOF){
+		ptr=line; count=1; result=0;
+		while(*ptr){
+			if(count>9)
+				break;
+			if(isdigit(*ptr)){
+				result+=((*ptr-48)*count);
+				count++;
+			}
+			ptr++;
 		}
-		return 0;
+		number=atoi(ptr+1);
+		if(result%11==number)
+			printf("Pass\n");
+		else
+			printf("Fail\n");
+	}
+	return 0;
 }
-
-
