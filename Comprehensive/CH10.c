c4b92df2e267a45bc60ca1554914d12c18680152
Wayne Chen <wayne861116@gmail.com>
2020-01-04 22:54:09 +0800

    Update and rename CH10.C to CH10.c

---
+++ Comprehensive/CH10.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	int i, a, n, state;
+	short *number;
+	while(scanf("%d", &a)!=EOF){
+		number=(short *)calloc(a+1, sizeof(short));
+		for(i=0; i<a-2; i++){
+			scanf("%d", &n);
+			number[n]=1;
+		}
+		state=0;
+		for(i=1; i<=a; i++){
+			if(!number[i]){
+				if(!state){
+					printf("%d ", i);
+					state=1;
+				}
+				else{
+					printf("%d\n", i);
+					break;
+				}
+			}
+		}
+		free(number);
+	}
+	return 0;
+}
