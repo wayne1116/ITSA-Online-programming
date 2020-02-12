1da47a26e70e2523474d774893ff92b4afc55ec4
Wayne Chen <wayne861116@gmail.com>
2020-02-12 22:49:30 +0800

    Update and rename ST29.C to ST29.c

---
+++ String/ST29.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<ctype.h>
+#define MAX 100000
+int main()
+{
+	int number, i, len;
+	char line[MAX], word[MAX];
+	char *ptr, *ptr1;
+	while(fgets(line, MAX, stdin)!=NULL){
+		len=strlen(line);
+		line[len-1]='\0';
+		ptr=line;
+		while(*ptr!='\0'){
+			if(isdigit(*ptr)){
+				number=atoi(ptr);
+				while(*ptr!='\0'){
+					if(!isdigit(*ptr))
+						break;
+					ptr++;
+				}
+				ptr1=ptr;
+				while(*ptr!='\0'){
+					if(isdigit(*ptr))
+						break;
+					ptr++;
+				}
+				strncpy(word, ptr1, ptr-ptr1);
+				word[ptr-ptr1]='\0';
+				for(i=0; i<number; i++)
+					printf("%s", word);
+			}
+		}
+		printf("\n");
+	}
+	return 0;
+}
