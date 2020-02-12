6410ed8a497b96688a51b8783b658f6f178f2093
Wayne Chen <wayne861116@gmail.com>
2020-02-12 22:34:58 +0800

    Update and rename ST38.C to ST38.c

---
+++ String/ST38.c
@@ -0,0 +1,53 @@
+#include<stdio.h>  
+#include<stdlib.h>  
+#include<ctype.h>  
+int main()  
+{  
+        char line[10000], *ptr, result[10000];  
+        int number, i, j, lcount, rcount, oper;  
+        fgets(line, 1024, stdin);  
+        ptr=line; i=0;  
+        while(*ptr){  
+                if(isspace(*ptr)){  
+                    ptr++;  
+                }  
+                else if(isdigit(*ptr)){  
+                        result[i++]=*ptr;  
+                        while(isdigit(*ptr))  
+                                ptr++;  
+                }  
+                else{  
+                        result[i++]=*ptr;  
+                        ptr++;  
+                }  
+        }  
+        lcount=rcount=number=oper=0;  
+        for(j=0; j<i; j++){  
+                if(result[j]=='(')  
+                        lcount++;  
+                else if(result[j]==')')  
+                        rcount++;  
+                else if(result[j]=='+' || result[j]=='-' || result[j]=='*' || result[j]=='/')  
+                        oper++;  
+                else  
+                        number++;  
+                if(lcount<rcount)  
+                        break;  
+                if(j>0){  
+                        if(result[j-1]=='(' && (result[j]==')' || result[j]=='+' || result[j]=='-' || result[j]=='*' || result[j]=='/'))  
+                                break;  
+                        else if(result[j-1]==')' && (result[j]=='(' || isdigit(result[j])))  
+                                break;  
+                        else if((result[j-1]=='+' || result[j-1]=='-' || result[j-1]=='*' || result[j-1]=='/') && (result[j]==')' ||  
+                                                result[j]=='+' || result[j]=='-' || result[j]=='*' || result[j]=='/'))  
+                                break;  
+                        else if(isdigit(result[j-1]) && (result[j]=='(' || isdigit(result[j])))  
+                                break;  
+                }  
+        }  
+        if(lcount!=rcount || oper+1!=number || j<i)  
+                printf("illegal equation\n");  
+        else  
+                printf("legal equation\n");  
+        return 0;  
+}  
