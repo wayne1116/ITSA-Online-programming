1875acc7c8845e290906586d8435c2edfb02197f
Wayne Chen <wayne861116@gmail.com>
2020-02-26 22:53:38 +0800

    Update and rename DP25.cpp to DP25.c

--- Dynamic_Programming/DP25.c
+++ Dynamic_Programming/DP25.c
@@ -4,9 +4,11 @@ int main()
 {  
     int result[92], i, a;  
     result[0]=1; result[1]=result[2]=2;  
+    
     for(i=3; i<92; i++){  
         result[i]=result[i-1]/4+result[i-2]/2+result[i-3];  
     }  
+    
     while(scanf("%d", &a)!=EOF){  
         printf("%d\n", result[a]);  
     }  
