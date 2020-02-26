736bd0cbb8614baffe4f90e3c32af66919b57f6c
Wayne Chen <wayne861116@gmail.com>
2020-02-26 23:03:36 +0800

    Update and rename DP39.C to DP39.c

---
+++ Dynamic_Programming/DP39.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	int a=0;
+	int i=0, j=0, k=0;
+	int max=0;
+	int map[11][11];
+	int area[11][11];
+	int result=0;
+	int state=0;
+	
+	scanf("%d", &a);
+	for(i=0; i<a; i++){
+		for(j=0; j<a; j++){
+			scanf("%d", &map[i][j]);
+			area[i][j]=0;
+		}
+	}
+	
+	state=0;
+	for(i=0; i<a; i++){
+		if(map[0][i]==0){
+			area[0][i]=1;
+			state=1;
+		}
+	}
+	
+	if(state) max=1;
+	else max=0;
+	
+	for(i=1; i<a; i++){
+		for(j=0; j<a; j++){
+			if(map[i][j]==0)
+				area[i][j]=area[i-1][j]+1;
+		}
+		
+		for(j=0; j<a; j++){
+			result=area[i][j];
+			for(k=j-1; k>=0 && area[i][k]>=area[i][j] ; k--)
+				result+=area[i][j];
+			
+			for(k=j+1; k<a && area[i][k]>=area[i][j] ; k++)
+				result+=area[i][j];
+			
+			if(max<result)
+				max=result;
+		}
+	}
+	
+	printf("%d\n", max);
+	return 0;
+}
